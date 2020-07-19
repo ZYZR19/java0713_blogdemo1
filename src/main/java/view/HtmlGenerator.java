ca6f18c97ef081a8096b33d0e872b0075802326e
ZYZR19 <1803255529@qq.com>
2020-07-19 22:04:08 +0800

    blog demo

---
+++ src/main/java/view/HtmlGenerator.java
@@ -0,0 +1,16 @@
+package view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: asus
+ * Date: 2020-07-19
+ * Time: 9:32
+ */
+public class HtmlGenerator {
+
+
+    public static String getMessagaPage(String register, String s) {
+        return null;
+    }
+}
